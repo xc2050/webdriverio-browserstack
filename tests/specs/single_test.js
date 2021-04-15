87de0ad5e26d18ad813e6faa062ec90d0f74be20
Rohan Chougule <rohan183chougule@gmail.com>
2021-04-15 19:48:29 +0530

    fix: mobile and desktop compatible script

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -1,8 +1,22 @@
-describe('Google\'s Search Functionality', () => {
-  it('can find search results', () => {
-      browser.url('https://www.google.com/ncr');
-      $('[name="q"]').setValue('BrowserStack');
-      $('[name="btnK"]').click();
-      browser.getTitle().should.match(/BrowserStack - Google Search/i);
+describe("Google's Search Functionality", () => {
+  it("can find search results", () => {
+    browser.url("https://www.google.com/ncr");
+    const inputForm = $('[name="q"]');
+    inputForm.setValue("BrowserStack");
+
+    try {
+      const submitButton = $('[name="btnK"]');
+      submitButton.waitForExist(8000);
+      submitButton.click(); // this works for desktop
+    } catch (e) {
+      browser.elementSubmit(inputForm.elementId); // this helps for mobile
+    }
+
+    browser.waitUntil(
+      () => browser.getTitle().match(/BrowserStack/i),
+      5000,
+      "Title didn't match with BrowserStack",
+      5
+    );
   });
 });
