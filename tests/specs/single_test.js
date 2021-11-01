abd77c51c06ca52f0812c89101c8603f0aa48e68
Anand Ganesh <anandganesh@Anands-MacBook-Pro.local>
2021-11-02 01:23:56 +0530

    ACE_3096, replaced synchronously written tests with async/ await to overcome node16 deprecation

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -1,17 +1,17 @@
 describe("Google's Search Functionality", () => {
-  it("can find search results", () => {
-    browser.url("https://www.google.com/ncr");
-    const inputForm = $("//input[@name='q']");
-    inputForm.setValue(["BrowserStack", "Enter"]); // this helps in majority desktops
+  it("can find search results", async () => {
+    await browser.url("https://www.google.com/ncr");
+    const inputForm = await $("//input[@name='q']");
+    await inputForm.setValue(["BrowserStack", "Enter"]); // this helps in majority desktops 
 
     try {
-      browser.waitUntil(() => browser.getTitle().match(/BrowserStack/i));
-    } catch (e) {
-      browser.elementSubmit(inputForm.elementId); // this helps in remaining cases, i.e. iPhone
+      (await browser.getTitle()).match(/BrowserStack/i);
+    } catch (e) {  
+      await browser.elementSubmit(inputForm.elementId); // this helps in remaining cases, i.e. iPhone
     }
 
-    browser.waitUntil(
-      () => browser.getTitle().match(/BrowserStack/i),
+    await browser.waitUntil(
+      async () => (await browser.getTitle()).match(/BrowserStack/i),
       5000,
       "Title didn't match with BrowserStack"
     );
