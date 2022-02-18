b4bd0d1095c0c97bbf1f498ddb4006e309a9e466
Saransh <saransh@browserstack.com>
2022-02-18 19:46:38 +0530

    removing local test file

--- tests/specs/local_test.js
+++
@@ -1,6 +0,0 @@
-describe('BrowserStack Local Testing', ()=> {
-  it('can check tunnel working', async () => {
-    await browser.url('http://bs-local.com:45691/check');
-    (await browser.getPageSource()).should.match(/Up and running/i);
-  });
-});
