bc206ce24e2a8e9ceb855d43ff78876697d84c57
Ankit Singh <2501.ankit@gmail.com>
2022-03-08 19:53:31 +0530

    add: new browserstack service in conf

--- conf/local-test.conf.js
+++ conf/local-test.conf.js
@@ -2,12 +2,11 @@ const { config: baseConfig }= require("./base.conf.js");
 
 const localConfig = {
   // Adding browserstackLocal to browserstack-service to initiate local binary
-  services: [['browserstack', { browserstackLocal: true, forcelocal: false }]], 
+  services: [['@browserstack/wdio-browserstack-service', { browserstackLocal: true, forcelocal: false }]], 
   capabilities: [{
     browserName: 'Chrome',
     "bstack:options": {
       buildName: 'browserstack-build-1',
-      local: true, // For enabling local testing
     }
   }],
   specs: ['./tests/specs/local_test.js']
