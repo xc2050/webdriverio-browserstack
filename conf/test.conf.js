af906232fd79f9c62597fffbfcccdb72ab0eb6ab
Ankit Singh <2501.ankit@gmail.com>
2022-03-09 19:17:58 +0530

    update: browser comabinations to run parallel tests on

--- conf/test.conf.js
+++ conf/test.conf.js
@@ -1,4 +1,4 @@
-const { config: baseConfig }= require("./base.conf.js");
+const { config: baseConfig } = require("./base.conf.js");
 
 const parallelConfig = {
   maxInstances: 10,
@@ -6,21 +6,24 @@ const parallelConfig = {
     build: 'browserstack-build-1',
   },
   capabilities: [{
-    browserName: 'chrome'
-  },{
-    browserName: 'firefox'
-  },{
-    browserName: 'internet explorer'
-  },{
-    browserName: 'safari'
-  },{
-    browserName: 'edge'
+    browserName: 'chrome',
+    browserVersion: 'latest',
+    os: 'Windows',
+    osVersion: '10'
+  }, {
+    browserName: 'safari',
+    browserVersion: 'latest',
+    os: 'OS X',
+    osVersion: 'Big Sur'
+  }, {
+    browserName: 'chrome',
+    device: 'Samsung Galaxy S20'
   }],
 };
 
-exports.config = {...baseConfig, ...parallelConfig};
+exports.config = { ...baseConfig, ...parallelConfig };
 
 // Code to support common capabilities
-exports.config.capabilities.forEach(function(caps){
-  for(var i in exports.config.commonCapabilities) caps[i] = caps[i] || exports.config.commonCapabilities[i];
+exports.config.capabilities.forEach(function (caps) {
+  for (var i in exports.config.commonCapabilities) caps[i] = caps[i] || exports.config.commonCapabilities[i];
 });
