49e4b37f15209a8ef3042d3344f8c3c44e4c18eb
Ankit Singh <2501.ankit@gmail.com>
2022-03-09 10:18:49 +0530

    chore: update local test conf

--- conf/local-test.conf.js
+++ conf/local-test.conf.js
@@ -4,10 +4,8 @@ const localConfig = {
   // Adding browserstackLocal to browserstack-service to initiate local binary
   services: [['@browserstack/wdio-browserstack-service', { browserstackLocal: true, forcelocal: false }]], 
   capabilities: [{
-    browserName: 'Chrome',
-    "bstack:options": {
-      buildName: 'browserstack-build-1',
-    }
+    browserName: 'chrome',
+    build: 'browserstack-build-1'
   }],
   specs: ['./tests/specs/local_test.js']
 };
