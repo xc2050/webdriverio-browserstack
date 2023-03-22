d9d85abf71b25ec9064185878b7a1f6331648995
Neha Agarwal <agarneha1331@gmail.com>
2023-03-22 19:59:05 +0530

    Increased timeout

--- conf/base.conf.js
+++ conf/base.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   screenshotPath: './errorShots/',
   baseUrl: '',
   waitforTimeout: 10000,
-  connectionRetryTimeout: 90000,
+  connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
   hostname: 'hub.browserstack.com',
   services: [['browserstack']],
